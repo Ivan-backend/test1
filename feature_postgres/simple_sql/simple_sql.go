769eee8f01bda4df96f470232053af78f6cce7a0
Ivan <ivanvavilov5704@gmail.com>
2026-03-13 02:28:36 +0400

    0.0.5

--- feature_postgres/simple_sql/simple_sql.go
+++ feature_postgres/simple_sql/simple_sql.go
@@ -2,6 +2,7 @@ package simple_sql
 
 import (
 	"context"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 )
@@ -22,3 +23,43 @@ func CreateTable(ctx context.Context, conn *pgx.Conn) error {
 
 	return err
 }
+
+func InsertRow(ctx context.Context, conn *pgx.Conn, serviceName string, price int, userId string, dateStart time.Time, dateEnd time.Time) error {
+	sqlQuery := `
+		INSERT INTO subscribes(servicename, price, userid, datestart, dateend)
+		VALUES($1, $2, $3, $4, $5);
+	`
+
+	_, err := conn.Exec(ctx, sqlQuery, serviceName, price, userId, dateStart, dateEnd)
+
+	return err
+}
+
+func GetSubscribes(ctx context.Context, conn *pgx.Conn) ([]SubscribeModel, error) {
+	sqlQuery := `
+		SELECT * FROM subscribes
+	`
+
+	rows, err := conn.Query(ctx, sqlQuery)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	subscribes := make([]SubscribeModel, 0)
+
+	for rows.Next() {
+		var subscribe SubscribeModel
+
+		err := rows.Scan(&subscribe.Id, &subscribe.ServiceName, &subscribe.Price, &subscribe.UserId, &subscribe.DateStart, &subscribe.DateEnd)
+		if err != nil {
+			return nil, err
+		}
+
+		subscribes = append(subscribes, subscribe)
+	}
+
+	return subscribes, nil
+}
+
