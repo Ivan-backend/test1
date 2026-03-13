bb05f772625d3e553f1063e01100319306603ec4
Ivan <ivanvavilov5704@gmail.com>
2026-03-13 14:41:33 +0400

    1.0.0

--- go.mod
+++ go.mod
@@ -143,6 +143,8 @@ require (
 	go.opentelemetry.io/otel/sdk v1.36.0 // indirect
 	go.opentelemetry.io/otel/sdk/metric v1.36.0 // indirect
 	go.opentelemetry.io/otel/trace v1.37.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.1 // indirect
 	golang.org/x/crypto v0.45.0 // indirect
 	golang.org/x/mod v0.29.0 // indirect
 	golang.org/x/net v0.47.0 // indirect
--- main.go
+++ main.go
@@ -10,19 +10,6 @@ import (
 	"main/users"
 )
 
-// @title Blueprint Swagger API
-// @version 1.0
-// @description Swagger API for Golang Project Blueprint.
-// @termsOfService http://swagger.io/terms/
-
-// @contact.name API Support
-// @contact.email martin7.heinz@gmail.com
-
-// @license.name MIT
-// @license.url https://github.com/MartinHeinz/go-project-blueprint/blob/master/LICENSE
-
-// @BasePath /api/v1
-
 func main() {
 	ctx := context.Background()
 	subscribes := subscribes.NewList()
